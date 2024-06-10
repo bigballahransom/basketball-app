ee0414c98a1e557a1afb156b640dcbf088d44756
bigballahransom <105400568+bigballahransom@users.noreply.github.com>
2024-06-10 12:21:54 -0700

    removed search and extra buttons from nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,7 @@ import { Button } from "./ui/button"
 
 const Header = () => {
   return (
-    <div className='flex items-center p-2 max-w-6xl mx-auto'>
+    <div className='flex items-center p-2 max-w-6xl mx-auto bg-white'>
       <Image
         className='rounded-lg'
         src='/images/basketball.png'
@@ -21,12 +21,12 @@ const Header = () => {
         height={40}
       />
       <div className='flex-1'>
-        <form className='flex items-center space-x-1 bg-gray-100 p-2 rounded-md flex-1 mx-2 max-w-96'>
+        {/* <form className='flex items-center space-x-1 bg-gray-100 p-2 rounded-md flex-1 mx-2 max-w-96'>
             <SearchIcon className='h-4 text-gray-600'/>
             <input type='text' placeholder='Search' className='bg-transparent flex-1 outline-none'/>
-        </form>
+        </form> */}
       </div>
-      <div className='flex items-center space-x-4 px-6'>
+      <div className=' items-center space-x-4 px-6 hidden md:flex'>
         <Link href='/' className='icon'>
             <HomeIcon className='h-6 text-gray-600'/>
             <p>Home</p>
