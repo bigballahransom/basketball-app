d97088d326c3a94216134259994bd019d27fa328
bigballahransom <105400568+bigballahransom@users.noreply.github.com>
2024-06-10 12:14:14 -0700

    may need to fix the flatmap on server in user information for production

--- app/page.tsx
+++ app/page.tsx
@@ -14,18 +14,18 @@ export default async function Home() {
   console.log(posts);
   return (
     <main className="grid grid-cols-8 mt-5 sm:px-5">
-      <section className='hidden md:inline md:col-span-2'>
+      {/* <section className='hidden md:inline md:col-span-2'>
         <UserInformation/>
-      </section>
+      </section> */}
       <section className="col-span-full md:col-span-6 xl:col-span-4 xl:max-w-xl mx-auto w-full px-1">
         <SignedIn>
           <PostForm/>
         </SignedIn>
           <PostFeed posts={posts}/>
       </section>
-      <section className="hidden xl:inline justify-center col-span-2">
+      {/* <section className="hidden xl:inline justify-center col-span-2">
         
-      </section>
+      </section> */}
     </main>
   );
 }
