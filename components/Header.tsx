4c545139b5198feb6a70bb5edb913c530df6295d
bigballahransom <105400568+bigballahransom@users.noreply.github.com>
2024-06-10 12:25:04 -0700

    adding nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -20,19 +20,19 @@ const Header = () => {
         width={40}
         height={40}
       />
-      <div className='flex-1'>
-        {/* <form className='flex items-center space-x-1 bg-gray-100 p-2 rounded-md flex-1 mx-2 max-w-96'>
+      {/*<div className='flex-1'>
+         <form className='flex items-center space-x-1 bg-gray-100 p-2 rounded-md flex-1 mx-2 max-w-96'>
             <SearchIcon className='h-4 text-gray-600'/>
             <input type='text' placeholder='Search' className='bg-transparent flex-1 outline-none'/>
-        </form> */}
-      </div>
-      <div className=' items-center space-x-4 px-6 hidden md:flex'>
+        </form>
+      </div>*/}
+      <div className=' items-center space-x-4 px-6 flex'>
         <Link href='/' className='icon'>
             <HomeIcon className='h-6 text-gray-600'/>
             <p>Home</p>
         </Link>
 
-        <Link href='/' className='icon hidden md:flex'>
+        <Link href='/' className='icon flex'>
             <Trophy className='h-6 text-gray-600 '/>
             <p>Tournaments</p>
         </Link>
